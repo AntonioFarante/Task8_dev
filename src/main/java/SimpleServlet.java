ccbe04b637d99d4e35c5e6c08ec227857f0cd49c
Antonio <oz0969991415@gmail.com>
2023-06-08 12:20:57 +0300

    Initial commit

---
+++ src/main/java/SimpleServlet.java
@@ -0,0 +1,21 @@
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class SimpleServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        resp.getWriter().write("Hello World");
+        resp.getWriter().close();
+    }
+}
